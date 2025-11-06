5d0487c32e5ac29581d5eab45d4555e49aecdd35
DanielGerardoHC <gerardoclm555@gmail.com>
2025-11-05 20:47:25 -0600

    Initial Commit

---
+++ src/main/java/com/biblioteca/sistema_gerencial_para_biblioteca/dao/impl_dao/UsuarioDAOImpl.java
@@ -0,0 +1,11 @@
+package com.biblioteca.sistema_gerencial_para_biblioteca.dao.impl_dao;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+
+public class UsuarioDAOImpl {
+    
+}
---
+++ src/main/java/com/biblioteca/sistema_gerencial_para_biblioteca/utils/JPAUtil.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.biblioteca.sistema_gerencial_para_biblioteca.utils;
+
+/**
+ *
+ * @author Daniel
+ */
+public class JPAUtil {
+    
+}
